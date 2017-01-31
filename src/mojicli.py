59801714fe5103f127dffc50643c655a85f88032
mathieu <mathieu.c.sabourin@gmail.com>
2017-01-30 19:54:15 -0800

    Implement filtering.

--- src/mojicli.py
+++ src/mojicli.py
@@ -1,7 +1,8 @@
 import argparse
 import json
 
-class Moji(object):
+
+class Mojira(object):
 
     def __init__(self, json_file: str) -> None:
         self.file = json_file
@@ -10,6 +11,9 @@ class Moji(object):
         with open(self.file) as fd:
             return json.load(fd)
 
+    def items(self) -> list:
+        return list(self.moji.items())
+
     @property
     def moji(self) -> dict:
         if not hasattr(self, '_moji'):
@@ -17,15 +21,30 @@ class Moji(object):
 
         return self._moji
 
-    def search(self, keyword) -> list:
+    def search(self, keyword: str) -> list:
         return [
             {'name': key, 'moji': res['char']}
-            for key, res in self.moji.items()
+            for key, res in self.items()
             if keyword in res['keywords']
         ]
 
+    def filter(self, keyword: str):
+        return MojiView(self, [k for k, v in self.items() if keyword in v['keywords']])
+
+
+class MojiView(Mojira):
+
+    def __init__(self, mojira: Mojira, keys: list) -> None:
+        self.mojira = mojira
+        self.keys = keys
+        self._moji = mojira._moji
+
+    def items(self) -> list:
+        return [(key, self.moji[key]) for key in self.keys]
+
+
 if __name__ == "__main__":
-    m = Moji('emojis.json')
+    m = Mojira('emojis.json')
 
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('search', type=str)
